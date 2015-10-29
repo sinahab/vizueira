ec38cfa25af0db2e7dd7f1db8037aba98119b7a0
stephen-standridge <stephen@formisfunction.io>
2015-10-29 15:36:01 -0700

    stable division using map

--- src/components/divisible.jsx
+++ src/components/divisible.jsx
@@ -11,31 +11,34 @@ var Divisible = React.createClass({
 
   render: function(){
     return (<g>
-            {this.state.cells.depthTraversalCall( function(arg){this.renderCell(arg)}.bind(this) )}
-            { console.log(this.state.cells)}
-            {this.state.cells.map( function( cell, index ){
+            {this.state.cells.map( function( cell ){
               if( cell !== undefined ){
-                return this.renderCell( cell, index )
+                return this.renderCell( cell )
               }
             }, this)
           }</g>)
   },
   createChildren: function( cell ){
-    let children = [];
-    let n = cell.n
-    let level = cell.level;
-
-    this.state.cells.goTo( n, level )
-
-    let size = this.state.cells.node.size / this.state.divisions;
-    for( var i = 0; i< this.state.divisions; i++ ){
-      children.push({ size: size, level: level + 1, n: this.state.cells.firstChildNode + i })
-    }
-    this.state.cells.children = children
+    this.state.cells.goTo( cell.n, cell.level )
+    this.state.cells.children = this.divide.call( this.state.cells.node )
     this.state.cells.root
     return this.state.cells
   },
-  handleClick: function(cell) {
+  divide: function() {
+    let children = [];    
+    let current = this.state.cells.node;
+    let size = current.size / this.state.divisions;    
+    for( var i = 0; i< this.state.divisions; i++ ){
+      children.push({ 
+        size: size, 
+        level: current.level + 1, 
+        n: this.state.cells.firstChildNode + i 
+      })
+    }  
+    return children  
+
+  },
+  handleClick: function( cell ) {
     this.setState({ cells: this.createChildren( cell ) })
   },
   renderCell: function( cell ){
